7bb6dcd4b6077476da74e90d991cf19dcdd45cce
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2021-12-16 10:20:42 -0600

    Initial commit

---
+++ spec/player_spec.rb
@@ -0,0 +1,10 @@
+require './lib/player'
+
+RSpec.describe Player do
+  it 'exists' do
+    player = Player.new({name: "Luka Modric", position: "midfielder"})
+
+    expect(player).to be_instance_of(Player)
+    expect(player.name).to eq("Luka Modric")
+    expect(player.position).to eq("midfielder")
+  end
