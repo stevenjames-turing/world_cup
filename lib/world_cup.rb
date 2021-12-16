10f59ea8de4190714e21dd553bfcc8b2f9d31ffa
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2021-12-16 12:57:14 -0600

    Feat: add #all_players_by_position. return a hash with key=position value=player object

--- lib/world_cup.rb
+++ lib/world_cup.rb
@@ -15,4 +15,18 @@ class WorldCup
     end
     active_players_by_position.flatten
   end
+
+  def all_players_by_position
+    all_players_by_position = {}
+    @teams.each do |team|
+      team.players.each do |player|
+        if all_players_by_position.has_key?(player.position) == false
+          all_players_by_position[player.position] = [] << player
+        else
+          all_players_by_position[player.position] << player
+        end
+      end
+    end
+    all_players_by_position
+  end
 end
--- spec/world_cup_spec.rb
+++ spec/world_cup_spec.rb
@@ -65,4 +65,23 @@ RSpec.describe WorldCup do
     expect(world_cup.active_players_by_position("forward")).to eq([mbappe])
     expect(world_cup.active_players_by_position("defender")).to eq([])
   end
+
+  it 'can list all players by position as a hash' do
+    france = Team.new("France")
+    croatia = Team.new("Croatia")
+    mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+    pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
+    modric = Player.new({name: "Luka Modric", position: "midfielder"})
+    vida = Player.new({name: "Domagoj Vida", position: "defender"})
+    france.add_player(mbappe)
+    france.add_player(pogba)
+    croatia.add_player(vida)
+    croatia.add_player(modric)
+    world_cup = WorldCup.new(2018, [france, croatia])
+
+    expect(world_cup.all_players_by_position.keys).to eq(["forward", "midfielder", "defender"])
+    expect(world_cup.all_players_by_position["forward"]).to eq([mbappe])
+    expect(world_cup.all_players_by_position["defender"]).to eq([vida])
+    expect(world_cup.all_players_by_position["midfielder"]).to eq([pogba, modric])
+  end
 end
