ef20f04d42ca6a002c5ba39dac9366c16ed715f8
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2021-12-16 10:29:42 -0600

    Add #initialize. Team accepts country as arg

--- lib/team.rb
+++ lib/team.rb
@@ -1,2 +1,7 @@
 class Team
-end 
+  attr_reader :country
+
+  def initialize(country)
+    @country = country
+  end 
+end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -3,5 +3,8 @@ require './lib/team'
 
 RSpec.describe Team do
   it 'exists' do
+    team = Team.new("France")
+
+    expect(team).to be_instance_of(Team)
   end
 end
